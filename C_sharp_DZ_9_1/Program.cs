054e61c366d47ccb2cd24d71ba1f1ba71e84e1ae
chmilrv@gmail.com <chmilrv@gmail.com>
2020-09-03 13:06:21 +0300

    1306

--- C_sharp_DZ_9_1/Program.cs
+++ C_sharp_DZ_9_1/Program.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Remoting.Lifetime;
 /*Разработать приложение «Тамагочи». Жизненный цикл персонажа — 1-2 минуты.
 Персонаж случайным образом выдаёт просьбы (но подряд одна и та же просьба не выдаётся).
 Просьбы могут быть следующие: Покормить, Погулять, Уложить спать, Полечить, Поиграть.
@@ -26,102 +27,60 @@ namespace C_sharp_DZ_9_1
     {
         public string Name { get; set; } = "Персонаж";
         public int LifeCount { get; set; }
-        Tama(string name, int lifeCount)
+        
+        public Tama(string name, int lifeCount)
         {
             Name = name;
             LifeCount = lifeCount;
         }
-        public void ShowTama()
+        public static void ShowTama()
         {
             WriteLine("(.^.)()()()()()()...");
         }
-        private void Life(Object source, System.Timers.ElapsedEventArgs e)
+        public static int Life()
         {
-            ShowTama();
-            WriteLine($"{Name} хочет есть. Жизней {LifeCount}.");
+            string[] requestArray = { "Покорми", "Погуляй", "Положи спать", "Поиграй", "Полечи" };
             Random _rand = new Random();
-            DialogResult result = MessageBox.Show("Покорми мня!!!", Name, MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
-            if (result == DialogResult.Yes)
-            {
-                WriteLine($"{Name} покормлен. Жизней {LifeCount}.");
-                
-            }
-            else
-            {
-                WriteLine($"{Name} непокормлен. Жизней {LifeCount}.");
-                
-            }
-            
+            string request = requestArray[_rand.Next(0, 5)];
+            DialogResult result = MessageBox.Show(request, Name, MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
+            if (result == DialogResult.Yes) return 1;
+            else return -1;
         }
-        //public static int WalkWithMe()
-        //{
-        //    DialogResult result = MessageBox.Show("Погуляй со мной!", Name, MessageBoxButtons.YesNo, MessageBoxIcon.Error);
-        //    if (result == DialogResult.Yes) return 1;
-        //    else return -1;
-        //}
-        //public static int PutMeToBad() 
-        //{
-        //    DialogResult result = MessageBox.Show("Положи спать!", Name, MessageBoxButtons.YesNo, MessageBoxIcon.Information);
-        //    if (result == DialogResult.Yes) return 1;
-        //    else return -1;
-        //}
-        //public static int PlayWithMe() 
-        //{
-        //    DialogResult result = MessageBox.Show("Поиграй со мной!", Name, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-        //    if (result == DialogResult.Yes) return 1;
-        //    else return -1;
-        //}
-        //public static int HealMe()
-        //{
-        //    DialogResult result = MessageBox.Show("Полечи меня!", Name, MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
-        //    if (result == DialogResult.Yes) return 1;
-        //    else return -1;
-        //}
+
         public void Kill(Object source, System.Timers.ElapsedEventArgs e)
         {
             MessageBox.Show("Конец!", Name, MessageBoxButtons.OK, MessageBoxIcon.Warning);
 
-            
+
         }
 
-    class Program
+        public static void LifeEvent(Object source, System.Timers.ElapsedEventArgs e)
         {
-            private static System.Timers.Timer aTimer;
-            private static void SetTimer(Tama pers)
-            {
+            ShowTama();
+            Life();
+        }
+
+
+    }
+    class Program
+    {
+        private static System.Timers.Timer aTimer;
+        static void Main(string[] args)
+        {
+                Title = "Тамагочи";
+                Tama pers = new Tama("Igor", 3);
                 aTimer = new System.Timers.Timer(2000);
-                aTimer.Elapsed += pers.Life;
+                aTimer.Elapsed += LifeEvent;
                 aTimer.AutoReset = true;
                 aTimer.Enabled = true;
                 
-            }
-            private static System.Timers.Timer killTimer;
-            private static void SetKillTimer(Tama pers)
-            {
-                killTimer = new System.Timers.Timer(15000);
-                killTimer.Elapsed += pers.Kill;
-                killTimer.AutoReset = false;
-                killTimer.Enabled = true;
-                
-            }
-
-
-            static void Main(string[] args)
-            {
-                Title = "Тамагочи";
-                Tama pers = new Tama("Igor", 3);
-                SetTimer(pers);
-                SetKillTimer(pers);
 
                 //do
                 //{
-                //    WriteLine($"Жизней до: {LifeCount}.");
-                //    LifeCount =+ TamaEventDo();
-                //    WriteLine($"Жизней после: {LifeCount}.");
                 //} while (LifeCount >= 0);
 
                 ReadKey();
-            }
         }
+        
     }
 }
